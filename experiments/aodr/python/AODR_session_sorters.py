523168547b690b5eca44323ef9b60a53c2cef5ac
lwthompson2 <45012464+lwthompson2@users.noreply.github.com>
2024-08-14 11:19:00 -0400

    Single channel sorting routine
    Will need to add pip install mountainsort5 in environment file

--- experiments/aodr/python/AODR_session_sorters.py
+++ experiments/aodr/python/AODR_session_sorters.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from pathlib import Path
 import platform
 import shutil
@@ -13,12 +14,13 @@ import spikeinterface.exporters as sexp
 import spikeinterface.curation as scur
 import spikeinterface.sortingcomponents as sc
 import spikeinterface.widgets as sw
-from probeinterface import generate_tetrode, ProbeGroup
+from probeinterface import generate_tetrode, ProbeGroup, Probe, generate_linear_probe
 from probeinterface.plotting import plot_probe
-import os
+import random, string, os
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
+
 
 class OpenEphysSessionSorter():
     """Read continuous signal data from an Open Ephys session.
@@ -59,7 +61,7 @@ class OpenEphysSessionSorter():
         self.out_folder = out_folder
         self.freq_min = freq_min
         self.freq_max = freq_max
-
+        '''
         for step in self.step_names:
             try:
                 func = self.__getattribute__(step)
@@ -67,6 +69,12 @@ class OpenEphysSessionSorter():
                 done = '...OK'
             except Exception as err:
                 done = f'...Fail, Error: {err}'
+            print(step, done)
+        '''
+        for step in self.step_names:
+            func = self.__getattribute__(step)
+            func()
+            done = '...OK'
             print(step, done)
 
     def clean_tree(self):
@@ -93,16 +101,75 @@ class OpenEphysSessionSorter():
         # set this to the recording
         self.recording = self.recording.set_probegroup(probegroup, group_mode='by_probe')
 
+    def set_linear(self):
+        linear_probe = generate_linear_probe(num_elec=len(self.channel_names), ypitch=30)
+        linear_probe.set_device_channel_indices(self.channel_names)
+        self.recording = self.recording.set_probe(linear_probe)
+
+    def set_single(self):
+        probe = Probe(ndim=2, si_units='um')
+        ch_positions = np.zeros((1, 2))
+        probe.set_contacts(positions=ch_positions, shapes='circle', shape_params={'radius': 5})
+        probe.set_device_channel_indices(self.channel_names)
+        self.recording = self.recording.set_probe(probe)
+
     def bandpass(self):
         # Filter w/bandpass
         self.recording = spre.bandpass_filter(recording=self.recording, freq_min=self.freq_min, freq_max=self.freq_max)
 
+    def single_ch_sorter_and_analyzer(self):
+        job_kwargs = dict(n_jobs=-1, progress_bar=True)
+        si.set_global_job_kwargs(**job_kwargs)
+        params = si.get_default_sorter_params('mountainsort5')
+        #params['clustering']['method'] = 'FeaturesClustering'
+        #params['matching']['method'] = 'circus'
+        params['detect_threshold'] = 5.5
+        params['filter'] = True  # For some reason,  making this false changes the detect sign. But leaving true and not using my preprocessing filter results in the data not being filtered?
+        self.sorting = si.run_sorter('mountainsort5', self.recording,
+                                            folder=self.out_folder+"/"+self.sorter_name, 
+                                            verbose=True, remove_existing_folder=True, **params)
+        #sorting = si.run_sorter(self.sorter_name, self.recording,
+        #                                    folder=self.out_folder+"/"+self.sorter_name, 
+        #                                    verbose=True, remove_existing_folder=True, **params)
+        self.sorting_analyzer = si.create_sorting_analyzer(self.sorting, self.recording,
+                                                    format="binary_folder", folder=self.out_folder+"/analyzer", 
+                                                    overwrite=True,
+                                                    **job_kwargs)
+        self.sorting_analyzer.compute("random_spikes", method="uniform", max_spikes_per_unit=500)
+        self.sorting_analyzer.compute("waveforms", **job_kwargs)
+        self.sorting_analyzer.compute("templates")
+        self.sorting_analyzer.compute("template_similarity")
+        self.sorting_analyzer.compute("noise_levels")
+        self.sorting_analyzer.compute("unit_locations", method="monopolar_triangulation")
+        self.sorting_analyzer.compute("correlograms", window_ms=100, bin_ms=5.)
+        self.sorting_analyzer.compute("isi_histograms")
+        self.sorting_analyzer.compute("principal_components", n_components=3, mode='by_channel_global', whiten=True, **job_kwargs)
+        self.sorting_analyzer.compute("quality_metrics", metric_names=["snr", "firing_rate"])
+        self.sorting_analyzer.compute("spike_amplitudes", **job_kwargs)
+
     def run_one_sorter_and_analyzer(self):
         # I believe a custom compute json-like file could also be used.
-        job_kwargs = dict(n_jobs=4, progress_bar=True, chunk_duration="1s")
+        job_kwargs = dict(n_jobs=1, progress_bar=True)
         si.set_global_job_kwargs(**job_kwargs)
-        sorting = si.run_sorter(self.sorter_name, self.recording, output_folder=self.out_folder+"/"+self.sorter_name, 
-                                verbose=True, remove_existing_folder=True)
+        params = si.get_default_sorter_params(self.sorter_name)
+        if self.sorter_name == 'tridesclous2':
+            params['apply_preprocessing'] = False           # do not filter and whiten data. spykingcircus always whitens
+            params['matching']['method'] = 'circus-omp-svd'
+            params['detection']['detect_threshold'] = 6
+            params['templates']['ms_before'] = 1
+            params['templates']['ms_after'] = 3
+            params['clustering']['method'] = 'FeaturesClustering'
+            params['clustering']['threshold_diff'] = 0.0001
+            params['selection']['min_n_peaks'] = 50000
+            params['clustering']['split_radius_um'] = 0
+            params['clustering']['merge_radius_um'] = 0
+        elif self.sorter_name == 'spykingcircus2':
+            params['apply_preprocessing'] = False
+            params['selection']['select_per_channel'] = True
+
+        sorting = si.run_sorter(self.sorter_name, self.recording,
+                                            folder=self.out_folder+"/"+self.sorter_name, 
+                                            verbose=True, remove_existing_folder=True, **params)
         sorting_analyzer = si.create_sorting_analyzer(sorting, self.recording,
                                                     format="binary_folder", folder=self.out_folder+"/analyzer", 
                                                     overwrite=True,
@@ -132,6 +199,7 @@ class OpenEphysSessionSorter():
 
     def open_phy():
         os.system("phy template-gui ./phy_example/params.py")
+
 
 
 
